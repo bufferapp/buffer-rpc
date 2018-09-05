d7231479ae793c36fab361778eb8be7fcb2a869b
Harrison Harnisch <hharnisc@gmail.com>
2018-09-05 16:08:23 -0500

    stub in list methods functionality

--- src/rpc.js
+++ src/rpc.js
@@ -1,4 +1,12 @@
 module.exports = () => (req, res) => {
+  if (req.body.name === 'methods') {
+    return res.send([
+      {
+        name: 'methods',
+        docs: 'list all available methods',
+      },
+    ])
+  }
   res.status(404).send({
     error: 'unknown method',
   })
--- test/rpc.test.js
+++ test/rpc.test.js
@@ -1,26 +1,54 @@
 const http = require('http')
 const express = require('express')
 const listen = require('test-listen')
+const bodyParser = require('body-parser')
 const request = require('request-promise')
 const rpc = require('../src/rpc')
 
 const createServer = handler => {
   const app = express()
-  app.get('*', handler)
+  app.use(bodyParser.json())
+  app.post('*', handler)
   return http.createServer(app)
 }
+
+const generateRequest = ({ url, name }) =>
+  request({
+    uri: url,
+    method: 'POST',
+    body: {
+      name,
+    },
+    json: true,
+  })
 
 describe('rpc', () => {
   it('should handle unknown method', async () => {
     expect.assertions(2)
     let url = await listen(createServer(rpc()))
     try {
-      await request(url)
+      await generateRequest({
+        url,
+        name: 'unknown',
+      })
     } catch (error) {
       expect(error.statusCode).toBe(404)
-      expect(JSON.parse(error.error)).toEqual({
+      expect(error.error).toEqual({
         error: 'unknown method',
       })
     }
+  })
+  it('should handle a request to methods', async () => {
+    let url = await listen(createServer(rpc()))
+    const body = await generateRequest({
+      url,
+      name: 'methods',
+    })
+    expect(body).toEqual([
+      {
+        name: 'methods',
+        docs: 'list all available methods',
+      },
+    ])
   })
 })
