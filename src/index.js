c442a9f18ec1e09e275bc205555a9a237e1b35a3
Harrison Harnisch <hharnisc@gmail.com>
2018-09-06 15:26:34 -0500

    handle a request with handled error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const rpc = require('./rpc')
 const method = require('./method')
+const createError = require('./createError')
 
 module.exports = {
   rpc,
   method,
+  createError,
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 const listen = require('test-listen')
 const { createServer, generateRequest } = require('./utils')
-const { rpc, method } = require('../src/')
+const { rpc, method, createError } = require('../src/')
 
 describe('index', () => {
   it('should handle a response', async () => {
@@ -12,5 +12,34 @@ describe('index', () => {
       name,
     })
     expect(body).toEqual({ result })
+  })
+
+  it('should handle a handled error response', async () => {
+    expect.assertions(2)
+    const name = 'someMethod'
+    const message = 'nope'
+    let url = await listen(
+      createServer(
+        rpc(
+          method(name, () => {
+            throw createError({
+              message,
+            })
+          }),
+        ),
+      ),
+    )
+
+    try {
+      await generateRequest({
+        url,
+        name,
+      })
+    } catch (error) {
+      expect(error.statusCode).toBe(400)
+      expect(error.error).toEqual({
+        error: message,
+      })
+    }
   })
 })
