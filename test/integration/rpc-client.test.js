a4dfdf9c105d41c1ea887fcd7e0f93587112cea1
Eduardo Esclapes <eduardo.esclapes@gmail.com>
2018-12-05 09:04:29 +0100

    Update version of client and add specific integration tests

--- test/integration/rpc-client.test.js
+++ test/integration/rpc-client.test.js
@@ -1,37 +1,29 @@
 const listen = require('test-listen')
 const Client = require('micro-rpc-client')
 const { createServer } = require('../utils')
-const { rpc, method } = require('../../src')
+const { rpc, method, errorMiddleware, createError } = require('../../src')
 
 const mockThrowingServer = rpc(
-  method('throws', () => {
+  method('unhandledError', () => {
     throw new Error('Mock Unexpected Error')
-  })
+  }),
+  method('handledError', () => {
+    throw createError({ message: 'Mock Handled Error' })
+  }),
+  method('customCode', () => {
+    throw createError({ message: 'Unauthorized', code: 1099 })
+  }),
+  method('401', () => {
+    throw createError({ message: 'Unauthorized', statusCode: 401 })
+  }),
 )
 
-const errorHandlerStyles = [
-  ['defaultErrorHandler', null],
-  [
-    'returnJsonWithErrorProperty',
-    (err, req, res, next) => {
-      res.status(err.statusCode || 500).json({ error: err.message })
-    },
-  ],
-  [
-    'returnJsonWithErrProperty',
-    (err, req, res, next) => {
-      res.status(err.statusCode || 500).json({ err: err.message })
-    },
-  ],
-]
-
-
-describe.each(errorHandlerStyles)('app: %s', (name, mockErrorHandler) => {
+describe('client integration', () => {
   let server
   let client
 
   beforeAll(async () => {
-    server = await createServer(mockThrowingServer, mockErrorHandler)
+    server = await createServer(mockThrowingServer, errorMiddleware)
     const url = await listen(server)
     client = new Client({ url })
   })
@@ -40,28 +32,53 @@ describe.each(errorHandlerStyles)('app: %s', (name, mockErrorHandler) => {
     server.close()
   })
 
-  it('returns the thrown error', () => {
-    const result = client.call('throws', {
-      testArg: ''
+  describe('unhandled errors', () => {
+    it('returns the thrown error', () => {
+      const result = client.call('unhandledError', { testArg: '' })
+
+      return expect(result).rejects.toThrow('Mock Unexpected Error')
     })
 
-    return expect(result).rejects.toThrow('Mock Unexpected Error')
+    it('returns a non-handled error', async () => {
+      let result
+
+      result = client.call('unhandledError', { testArg: '' })
+
+      return expect(result).rejects.toHaveProperty('handled', false)
+    })
+
+    it('returns a status code', async () => {
+      let result
+
+      result = client.call('unhandledError', { testArg: '' })
+
+      return expect(result).rejects.toHaveProperty('status', 500)
+    })
   })
 
-  it('returns a non-handled error', async () => {
-    let result
-    let error
+  describe('handled errors', () => {
+    it('returns a handled error', async () => {
+      let result
 
-    try{
-      result = await client.call('throws', {
-        testArg: ''
-      })
-    } catch (e) {
-      error = e
-    }
+      result = client.call('handledError', { testArg: '' })
 
-    expect(result).toBe(undefined)
-    expect(error).toBeDefined()
-    expect(error.handled).toBeFalsy()
+      return expect(result).rejects.toHaveProperty('handled', true)
+    })
+
+    it('returns a custom code error', async () => {
+      let result
+
+      result = client.call('customCode', { testArg: '' })
+
+      return expect(result).rejects.toHaveProperty('code', 1099)
+    })
+
+    it('returns a specific status code', async () => {
+      let result
+
+      result = client.call('401', { testArg: '' })
+
+      return expect(result).rejects.toHaveProperty('status', 401)
+    })
   })
 })
